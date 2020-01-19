6db93963cf6c02a08a1f48bff4986bde1b070877
Nishant Kohli <nishkohli96@gmail.com>
2020-01-19 21:49:26 +0530

    Added  Fullcalendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AuthService } from './auth.service';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FullcalendarComponent } from './fullcalendar/fullcalendar.component'; 
+
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ProfileComponent
+    ProfileComponent,
+    FullcalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule 
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
---
+++ src/app/fullcalendar/fullcalendar.component.ts
@@ -0,0 +1,67 @@
+
+import { Component,ViewChild, OnInit } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+@Component({
+  selector: 'app-fullcalendar',
+  templateUrl: './fullcalendar.component.html',
+  styleUrls: ['./fullcalendar.component.css']
+})
+export class FullcalendarComponent implements OnInit {
+
+ // @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
+
+  calendarVisible = true;
+  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+  calendarWeekends = true;
+  calendarEvents: EventInput[] = [
+    { title: 'Event Now', start: new Date(),end: new Date() }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleVisible() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends() {
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  // gotoPast() {
+  //   let calendarApi = this.calendarComponent.getApi();
+  //   calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
+  // }
+
+  addEvent() {
+    this.calendarEvents = this.calendarEvents.concat({ // creates a new array!
+       title: 'event 2', date: '2020-04-02' 
+    });
+  }
+
+  modifyTitle(eventIndex, newTitle) {
+    let calendarEvents = this.calendarEvents.slice(); // a clone
+    let singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
+    singleEvent.title = newTitle;
+    calendarEvents[eventIndex] = singleEvent;
+    this.calendarEvents = calendarEvents; // reassign the array
+  }
+
+  handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: 'New Event',
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+  }
+
+}
